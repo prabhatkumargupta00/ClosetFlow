d0a7d7437b8d3fa84c9339ce830c7a923b09cec9
Prabhat kumar gupta <168262384+prabhatkumargupta00@users.noreply.github.com>
2026-04-08 17:53:32 +0530

    fix Cloudflare 521 error

--- app.js
+++ app.js
@@ -168,6 +168,6 @@ app.use((err, req, res, next) => {
 
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is listening on port ${PORT}`);
 });
